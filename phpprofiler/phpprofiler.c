6202434e2d4c33acd2c417a1985520f8b8d1b00b
Andrey Baboshin <andrey.baboshin@gmail.com>
2020-09-09 22:22:01 +0200

    cleanup

--- phpprofiler/phpprofiler.c
+++ phpprofiler/phpprofiler.c
@@ -15,37 +15,6 @@
 	ZEND_PARSE_PARAMETERS_END()
 #endif
 
-/* {{{ void phpprofiler_test1()
- */
-PHP_FUNCTION(phpprofiler_test1)
-{
-	ZEND_PARSE_PARAMETERS_NONE();
-
-	php_printf("The extension %s is loaded and working!\r\n", "phpprofiler");
-}
-/* }}} */
-
-/* {{{ string phpprofiler_test2( [ string $var ] )
- */
-PHP_FUNCTION(phpprofiler_test2)
-{
-	char *var = "World";
-	size_t var_len = sizeof("World") - 1;
-	zend_string *retval;
-
-	ZEND_PARSE_PARAMETERS_START(0, 1)
-		Z_PARAM_OPTIONAL
-		Z_PARAM_STRING(var, var_len)
-	ZEND_PARSE_PARAMETERS_END();
-
-	retval = strpprintf(0, "Hello %s", var);
-
-	RETURN_STR(retval);
-}
-/* }}}*/
-
-/* {{{ PHP_RINIT_FUNCTION
- */
 PHP_RINIT_FUNCTION(phpprofiler)
 {
 #if defined(ZTS) && defined(COMPILE_DL_PHPPROFILER)
@@ -54,44 +23,28 @@ PHP_RINIT_FUNCTION(phpprofiler)
 
 	return SUCCESS;
 }
-/* }}} */
 
-/* {{{ PHP_MINFO_FUNCTION
- */
+PHP_MINIT_FUNCTION(phpprofiler)
+{
+	return SUCCESS;
+}
+
 PHP_MINFO_FUNCTION(phpprofiler)
 {
 	php_info_print_table_start();
 	php_info_print_table_header(2, "phpprofiler support", "enabled");
 	php_info_print_table_end();
 }
-/* }}} */
 
-/* {{{ arginfo
- */
-ZEND_BEGIN_ARG_INFO(arginfo_phpprofiler_test1, 0)
-ZEND_END_ARG_INFO()
-
-ZEND_BEGIN_ARG_INFO(arginfo_phpprofiler_test2, 0)
-	ZEND_ARG_INFO(0, str)
-ZEND_END_ARG_INFO()
-/* }}} */
-
-/* {{{ phpprofiler_functions[]
- */
 static const zend_function_entry phpprofiler_functions[] = {
-	PHP_FE(phpprofiler_test1,		arginfo_phpprofiler_test1)
-	PHP_FE(phpprofiler_test2,		arginfo_phpprofiler_test2)
 	PHP_FE_END
 };
-/* }}} */
 
-/* {{{ phpprofiler_module_entry
- */
 zend_module_entry phpprofiler_module_entry = {
 	STANDARD_MODULE_HEADER,
 	"phpprofiler",					/* Extension name */
 	phpprofiler_functions,			/* zend_function_entry */
-	NULL,							/* PHP_MINIT - Module initialization */
+	PHP_MINIT(phpprofiler),							/* PHP_MINIT - Module initialization */
 	NULL,							/* PHP_MSHUTDOWN - Module shutdown */
 	PHP_RINIT(phpprofiler),			/* PHP_RINIT - Request initialization */
 	NULL,							/* PHP_RSHUTDOWN - Request shutdown */
@@ -99,7 +52,6 @@ zend_module_entry phpprofiler_module_entry = {
 	PHP_PHPPROFILER_VERSION,		/* Version */
 	STANDARD_MODULE_PROPERTIES
 };
-/* }}} */
 
 #ifdef COMPILE_DL_PHPPROFILER
 # ifdef ZTS
